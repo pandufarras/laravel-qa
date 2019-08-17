244a678b9890c4be4993017da2342854b08dac4c
Pandu Farras <pandufarras@gmail.com>
2019-08-17 20:48:10 +0700

    Generate Question model via artisan model generator

--- app/User.php
+++ app/User.php
@@ -36,4 +36,15 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function questions()
+    {
+        return $this->hasMany(Question::class);
+    }
+
+    public function setTittleAttribute($value)
+    {
+        $this->attribute['title'] = $value;
+        $this->attribute['slug'] = str_slug($value);
+    }
 }
