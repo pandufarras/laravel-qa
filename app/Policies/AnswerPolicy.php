3368717d0c7ca68521feb6556df0cb6fd139cf99
Pandu Farras <pandufarras@gmail.com>
2019-08-19 19:45:31 +0700

    Implement edit answer on question

---
+++ app/Policies/AnswerPolicy.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Policies;
+
+use App\User;
+use App\Answer;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class AnswerPolicy
+{
+    use HandlesAuthorization;
+
+
+    public function update(User $user, Answer $answer)
+    {
+        return $user->id == $answer->user_id;
+    }
+
+    /**
+     * Determine whether the user can delete the answer.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Answer  $answer
+     * @return mixed
+     */
+    public function delete(User $user, Answer $answer)
+    {
+        return $user->id == $answer->user_id;
+    }
+
+
+}
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,11 @@
 
 namespace App\Providers;
 
+use App\Answer;
+use App\Policies\AnswerPolicy;
 use App\Question;
 use App\Policies\QuestionPolicy;
+
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
@@ -16,6 +19,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         Question::class => QuestionPolicy::class,
+        Answer::class => AnswerPolicy::class,
     ];
 
     /**
